3e90cddbebec73cd394077de19a3474879664435
wbord <wbord@192.168.15.4>
2016-08-04 22:39:37 -0300

    commit um

---
+++ Cadastros/cad_produtor.php
@@ -0,0 +1,69 @@
+<?php
+
+
+?>
+
+<html>
+    <meta charset="utf-8">
+    <title>Cadastro de Produtor</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="../css/Flat-UI-master/dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="../css/Flat-UI-master/dist/css/flat-ui.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="../css/Flat-UI-master/dist/img/favicon.ico">
+    <head>
+        
+    </head>
+    <body>
+        <h3>Cadastro de Produtor</h3>
+        <fieldset>
+         <div class="container">
+            <h4>Forms</h4>
+            <div class="row">
+              <div class="col-md-12">
+                <h4>Basic Form</h4>
+                <form role="form">
+                  <legend>Form Legend</legend>
+                  <div class="form-group">
+                    <label for="exampleInputNome">Nome do Produtor</label>
+                    <input type="nome" class="form-control" id="exampleInputNome" placeholder="Nome do Produtor">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputTelefone">Telefone do Produtor</label>
+                    <input type="nome" class="form-control" id="exampleInputTelefone" placeholder="Telefone do Produtor">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEndereco">Endereço do Produtor</label>
+                    <input type="nome" class="form-control" id="exampleInputNome" placeholder="Bairro,Rua,Número">
+                  </div>
+                  <div class="form-group">
+                    <select data-toggle="select" class="form-control select select-default">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Nome do Produtor</label>
+                    <input type="nome" class="form-control" id="exampleInputNome" placeholder="Nome do Produtor">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>                               
+                  <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+              </div><!-- /.col-md-12 -->
+            </div><!-- /.row -->
+        </div>
+    </body>
+</html>
+
+
+  
